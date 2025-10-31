21759fdaa558d320ef21e42d0220e22ce3008f92
lancer224 <goblincharbelcher4@gmail.com>
2025-10-31 08:02:12 +0300

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -14,14 +14,10 @@ from sklearn.metrics import accuracy_score
 
 
 df = pd.read_csv(r"C:\Users\gobli\Documents\GitHub\AI_lab\lab1\PythonProject1\processed_titanic_train.csv")
-#_columns_drop = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
-#df = df.drop(columns=_columns_drop, errors='ignore')
 X = df.drop(['Age'], axis = 1)
 y = df['Age']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 37)
-
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.4, random_state=37)
 
 linear_model.fit(X_train, y_train)
 y_pred_test = linear_model.predict(X_test)
@@ -36,8 +32,7 @@ print(" MAE: ", MAE)
 
 X = df.drop(['Transported'], axis = 1)
 y = df['Transported']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 37)
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.4, random_state=37)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 33)
 
 logregmodel.fit(X_train, y_train)
 y_pred_test = logregmodel.predict(X_test)
